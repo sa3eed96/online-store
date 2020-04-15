4c1d261fb22b7f9884917b593120f2348ebbf20d
unknown <saied.alabbar@gmail.com>
2020-04-16 01:08:39 +0200

    set mergeparams to false  in router

--- routes/rate.js
+++ routes/rate.js
@@ -2,7 +2,7 @@ const express = require('express');
 const authController = require('../controllers/authentication');
 const rateController = require('../controllers/rate');
 const validation = require('../validations/rate');
-const router = express.Router();
+const router = express.Router({ mergeParams: true });
 
 router.put('/:rateId', validation('update'), authController.checkAuthentication, rateController.update);
 
